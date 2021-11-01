95e14b0b861c03e49511d1022db8684105f1c84e
Santu2002 <90022166+Santu2002@users.noreply.github.com>
2021-11-01 19:26:49 +0530

    Add files via upload

---
+++ tscript.js
@@ -0,0 +1,14 @@
+var x;
+console.log(x);
+
+if (x == undefined){
+ console.log("x is undefined");
+ }
+ 
+ x=200;
+ if(x == undefined){
+ console.log("x is undefined");
+ }
+ else {
+  console.log("x has been defined");
+  }
